55afe8b33be054892898d1d267b318972b37ae7a
Efim Sivolap <efim.sivolap@gmail.com>
2022-07-15 11:34:18 +0500

    mongoose mongoDB

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://efim:Minimalist2012@cluster0.44kxr.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error", err));
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("hello efim");
+});
+
+app.post("/auth/login", (req, res) => {
+  console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullNmae: "Efim Sivolap",
+    },
+    "secret123"
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
 });
 
 app.listen(4444, (err) => {
