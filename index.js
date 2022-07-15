911b16499872b64522e4ababf955e0d44515e1bf
Efim Sivolap <efim.sivolap@gmail.com>
2022-07-15 10:17:42 +0500

    nodemon

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import express from "express";
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello efim");
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("server ok");
+});
